9ca4279abfa06cd9935373c43d9094f60cad508f
Junha Jang <jangjunha113@gmail.com>
2016-07-11 21:44:27 +0900

    SkillBook Scene

---
+++ Assets/Scripts/Framework/SkillBookController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillBookController : MonoBehaviour {
+	[SerializeField]
+	private GameObject skillCellPrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 6; i++) {
+			var cell = Instantiate (skillCellPrefab) as GameObject;
+			var skill = cell.GetComponent<Skill>();
+			cell.transform.SetParent (this.gameObject.transform, false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
