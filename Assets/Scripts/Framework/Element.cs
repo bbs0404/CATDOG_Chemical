ffffca2f4fc78b1de23b4bcdf58c3b647feaae92
Junha Jang <jangjunha113@gmail.com>
2016-07-15 22:42:24 +0900

    Create ElementManager.

---
+++ Assets/Scripts/Framework/Element.cs
@@ -0,0 +1,11 @@
+﻿public struct Element {
+	public char symbol;
+	public int damage;
+	public int cost;
+
+	public Element(char symbol, int damage, int cost) {
+		this.symbol = symbol;
+		this.damage = damage;
+		this.cost = cost;
+	}
+}
