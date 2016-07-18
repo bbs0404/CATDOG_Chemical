2a0d52c150fae49a000d5d01890c407a6bfb93d0
tkdwns159 <tkdwns159@naver.com>
2016-07-18 12:26:03 +0900

    가방 버튼 UI

---
+++ Assets/Scripts/Framework/SceneManagerOfInventory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerOfInventory : MonoBehaviour {
+
+    
+    public void OnGUI(int a)
+    {
+       
+        SceneManager.UnloadScene(a);
+        SceneManager.LoadSceneAsync(a,LoadSceneMode.Additive);
+        Debug.Log("성공!");
+    }
+}
