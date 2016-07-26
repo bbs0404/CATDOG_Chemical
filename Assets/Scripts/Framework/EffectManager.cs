6042c71a0d64bbf93d2b857d0fb7135ebfbaba1f
bsh0404 <bsh04054@naver.com>
2016-07-27 01:18:14 +0900

    TitileScene

---
+++ Assets/Scripts/Framework/EffectManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectManager : SingletonBehaviour<EffectManager> {
+
+    public void EffectCreate(GameObject effect, Vector2 pos, float time)
+    {
+        GameObject newEffect =Instantiate(effect);
+        newEffect.transform.position = pos;
+        StartCoroutine(EffectPlay(effect, time));
+    }
+
+    public IEnumerator EffectPlay(GameObject effect, float time)
+    {
+
+        while (time > 0)
+        {
+            time -= GameTime.deltaTime;
+            yield return null;
+        }
+
+        Destroy(effect);
+
+        yield return null;
+    }
+}
