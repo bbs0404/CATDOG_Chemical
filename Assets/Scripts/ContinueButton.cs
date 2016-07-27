6fd4ade4b09bfbe2a2c77b5b7ce116f3762df039
unknown <>
2016-07-27 17:18:36 +0900

    타이틀씬 버튼생성

---
+++ Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Events;
+
+public class ContinueButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnClick()
+    {
+        SceneManager.LoadScene("InGame");
+        Debug.Log("Button Clicked!!");
+    }
+
+}
