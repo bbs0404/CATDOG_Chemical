da5d192a04e2102025c472c886669f3ef115e11b
bsh0404 <bsh04054@naver.com>
2016-08-25 18:35:45 +0900

    make player level + exp saved

--- Assets/Scripts/Framework/PlayerManager.cs
+++ Assets/Scripts/Framework/PlayerManager.cs
@@ -1,35 +1,56 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class PlayerManager : SingletonBehaviour<PlayerManager> {
 
     [SerializeField]
     private ObjectPlayer player;
-
+    [SerializeField]
     public static int playerLevel = 0;
     public static int playerEXP = 0;
     public static int villageProgress = 0; //플레이어의 최대 마을거리
 
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "InGame(Laziu)" && player == null)
+        {
+            player = GameObject.FindObjectOfType<ObjectPlayer>();
+            PlayerStatUpdate();
+        }
+    }
+
     void Start()
     {
+        SetStatic();
+        player = GameObject.FindObjectOfType<ObjectPlayer>();
         //Load Data from Save
-        int hp = 500, atk = 300, def = 300, exp = 100;
-        for (int i = 0; i < playerLevel; ++i)
+        PlayerStatUpdate();
+    }
+
+    public void PlayerStatUpdate()
+    {
+        if (player != null)
         {
-            hp += (int)(i * 7 + player.getDefend() * 0.1);
-            atk += (int)(player.getAttack() * 0.1 + i * 15.7);
-            def += (int)(i * 13 + player.getDefend() * 0.07);
-            exp += (int)(i * 25.7 + 53);
+            int hp = 500, atk = 300, def = 300, exp = 100;
+            for (int i = 0; i < playerLevel; ++i)
+            {
+                hp += (int)(i * 7 + player.getDefend() * 0.1);
+                atk += (int)(player.getAttack() * 0.1 + i * 15.7);
+                def += (int)(i * 13 + player.getDefend() * 0.07);
+                exp += (int)(i * 25.7 + 53);
+            }
+            player.setMaxHP(hp);
+            player.setHP(hp);
+            player.setAttack(atk);
+            player.setDefend(def);
+            player.setEXPtoLevelUP(exp);
+            player.setLevel(playerLevel);
+            player.setEXP(playerEXP);
+            player.getLVtext().text = "LV." + (player.getLevel() + 1).ToString();
         }
-        player.setMaxHP(hp);
-        player.setHP(hp);
-        player.setAttack(atk);
-        player.setDefend(def);
-        player.setEXPtoLevelUP(exp);
-        player.setLevel(playerLevel);
-        player.setEXP(playerEXP);
     }
 
     public ObjectPlayer getPlayer()
